59d86a9b556d4811144d4454c5adb15f5cdd4df5
greymon10 <alessandrogrimon3@gmail.com>
2026-07-21 00:49:40 +0100

    Create db_config.php

---
+++ includes/db_config.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        'ip'          => '127.0.0.1',
+        'puerto'      => 3306,
+        'usuario'     => 'root',
+        'contrasenia' => 'Tito2013-',
+        'db_nombre'   => 'users',
+    ];
